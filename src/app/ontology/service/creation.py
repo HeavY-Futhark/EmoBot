0688b113ff76a85d308a534614eaebf897f83ce2
MoonLyss <shaitan.lyssm@gmail.com>
2023-11-17 16:51:07 +0100

    fix import

--- src/app/ontology/service/creation.py
+++ src/app/ontology/service/creation.py
@@ -4,4 +4,5 @@ from rdflib import Graph, Namespace
 def create_emonto() -> Graph:
     g = Graph()
     EMO = Namespace("http://www.emonto.org/ (accessed on 18 December 2020)") 
+    # g.add()
     return g
