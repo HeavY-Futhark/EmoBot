abf9c435c1cf61dfcb287218eacc9794c2d0bb47
HeavY-Futhark <hugomosser64@gmail.com>
2023-12-28 03:35:13 +0100

    Added Model V1.2 (might be functionnal now)

--- src/app/models/train_model.py
+++ src/app/models/train_model.py
@@ -25,16 +25,18 @@ def preprocess_text(raw_tweet):
 def preprocess_semeval_data(dataset, transformer_model):
     # Extract tweet texts
     texts = [preprocess_text(example['Tweet']) for example in dataset['train']]
-
+    
     # Extract emotion labels
     emotion_labels = ['anger', 'anticipation', 'disgust', 'fear', 'joy', 'love', 'optimism', 'pessimism', 'sadness', 'surprise', 'trust']
     emotions = []
     for example in tqdm(dataset['train'], desc="Extracting Emotions"):
         example_emotions = [example[emotion] for emotion in emotion_labels]
         emotions.append(example_emotions)
+       
 
     # Convert texts to embeddings using the provided transformer model
-    text_embeddings = transformer_model.encode(texts)
+    text_embeddings = transformer_model.encode(texts, show_progress_bar=True)
+
 
     return text_embeddings, np.array(emotions)
 
@@ -58,6 +60,7 @@ def create_mlp_models(transformers):
     return mlps
 
 def train_models(mlps, X_train, y_train):
+    print("training...")
     for idx, mlp in enumerate(tqdm(mlps, desc="Training Models")):
         print(f"Training MLP {idx+1}/{len(mlps)}...")
         mlp.fit(X_train, y_train)
@@ -90,6 +93,7 @@ if __name__ == "__main__":
     # Preprocess the data using the function from preprocess.py
     X, y = preprocess_semeval_data(dataset, transformer_model)
 
+    print("preprocess ok")
     # Split data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 
@@ -100,4 +104,4 @@ if __name__ == "__main__":
     evaluate_models(mlps, X_test, y_test)
 
     # Save the trained models
-    save_models(mlps, 'src/models/saved_models')
+    save_models(mlps, 'saved_models')
